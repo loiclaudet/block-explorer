62562cc8d8a857a5324459c03c10215d1768961f
Lodz <8344874+0xLodz@users.noreply.github.com>
2022-12-20 17:46:29 +0100

    feat: use the next 13 app directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css"; // These styles apply to every route in the application
 import Head from "./head";
+import { Inter } from "@next/font/google";
+const inter = Inter({
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <Head />
       <body>{children}</body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Addition of the `app` directory for Next.js 13.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+      },
+    },
   },
   plugins: [],
 };
