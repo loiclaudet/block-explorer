729ff20ccfa32b6881c663d2a100839878958dfc
Lodz <8344874+0xLodz@users.noreply.github.com>
2022-12-21 08:10:50 +0100

    feat: add colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en" className={`${inter.variable} bg-warning-100`}>
       <Head />
       <body>{children}</body>
     </html>
