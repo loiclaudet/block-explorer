a803476bbf49741e76aea80a36e088384daca0b6
Lodz <8344874+0xLodz@users.noreply.github.com>
2022-12-29 15:40:54 +0100

    feat: add some og meta data

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,29 @@ export default function Head() {
     <>
       <title>Block Explorer</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="Ethereum block explorer" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="description"
+        content="Ethereum block explorer, Alchemy University week3 project"
+      />
+      <meta property="og:title" content="Ethereum Scan 🔎 | AU" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:description"
+        content="Ethereum block explorer, Alchemy University week3 project"
+      />
+      <meta property="og:image" content="/og-image.png" />
+      <meta property="og:url" content="/" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Ethereum Scan 🔎 | AU" />
+      <meta
+        name="twitter:description"
+        content="Ethereum block explorer, Alchemy University week3 project"
+      />
+      <meta name="twitter:site" content="@0xLodz" />
+      <meta name="twitter:image" content="/og-image.png" />
+      <meta name="twitter:creator" content="@0xLodz" />
+      <link rel="icon" href="/favicon-32x32.png" />
     </>
   );
 }
